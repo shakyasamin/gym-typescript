c5a6e0235ca2f41d38096347e0f3063d19e2280f
shakyasamin <saminshakya27@gmail.com>
2023-01-12 12:08:56 +0545

    nav bar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
+SelectedPage;
+
 function App() {
-  return <div className="App">app</div>;
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  return (
+    <div className="app bg-gray-20 ">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
