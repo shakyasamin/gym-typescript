0bd1be5c50fb84427b76592433b02be75f19e6f8
shakyasamin <saminshakya27@gmail.com>
2023-01-13 15:47:21 +0545

    HomePageText image position relative to another

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -17,13 +17,13 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
       {/* IMAGE AND MAIN HEADER */}
-      <div>
+      <div className="mx-auto w-5/6 items-center justify-center md:flex md:h-5/6 ">
         {/* MAIN HEADER */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADING */}
-          <div>
-            <div>
-              <div>
+          <div className="md:-md-20">
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
